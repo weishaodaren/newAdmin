e1879aae6cdc15d03adbb8508cc5fbffaf50c731
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-02-27 12:01:12 +0800

    denug

--- hotpot_admin.sql
+++ hotpot_admin.sql
@@ -1,20 +1,20 @@
 SET NAMES UTF8;
-DROP DATABASE IF EXISTS xiaofeiniu;
-CREATE DATABASE xiaofeiniu CHARSET=UTF8;
-USE xiaofeiniu;
+DROP DATABASE IF EXISTS hotpot;
+CREATE DATABASE hotpot CHARSET=UTF8;
+USE hotpot;
 
 /*管理员*/
-CREATE TABLE xfn_admin(
+CREATE TABLE hp_admin(
   aid INT PRIMARY KEY AUTO_INCREMENT,
   aname VARCHAR(32) UNIQUE,
   apwd VARCHAR(64)
 );
-INSERT INTO xfn_admin VALUES
+INSERT INTO hp_admin VALUES
 (NULL, 'admin', PASSWORD('123456')),
 (NULL, 'boss', PASSWORD('999999'));
 
 /*全局设置*/
-CREATE TABLE xfn_settings(
+CREATE TABLE hp_settings(
   sid INT PRIMARY KEY AUTO_INCREMENT,
   appName VARCHAR(32),
   apiUrl VARCHAR(64),
@@ -23,33 +23,33 @@ CREATE TABLE xfn_settings(
   icp VARCHAR(64),
   copyright VARCHAR(128)
 );
-INSERT INTO xfn_settings VALUES
-(NULL, '小肥牛', 'http://127.0.0.1:8090', 'http://127.0.0.1:8091', 'http://127.0.0.1:8092', '京ICP备12003709号-3', 'Copyright © 北京达内金桥科技有限公司版权所有');
+INSERT INTO hp_settings VALUES
+(NULL, '巴啦啦老魔仙', 'http://127.0.0.1:8090', 'http://127.0.0.1:8091', 'http://127.0.0.1:8092', 'XICP备XXXXXX号', 'Copyright © XXXXXXXX有限公司版权所有');
 
 /*桌台表*/
-CREATE TABLE xfn_table(
+CREATE TABLE hp_table(
   tid INT PRIMARY KEY AUTO_INCREMENT,
   tname VARCHAR(32),
   type VARCHAR(32),
   status INT
 );
-INSERT INTO xfn_table VALUES
-(1, '金镶玉', '2人桌', 1),
-(2, '玉如意', '2人桌', 1),
-(3, '齐天寿', '6人桌', 3),
-(5, '福临门', '4人桌', 2),
-(6, '全家福', '6人桌', 3),
-(7, '展宏图', '2人桌', 1),
-(8, '万年长', '8人桌', 1),
-(9, '百事通', '4人桌', 3),
-(10, '满堂彩', '10人桌', 2),
-(11, '鸿运头', '8人桌', 1),
-(12, '福满堂', '12人桌', 1),
-(13, '高升阁', '4人桌', 3),
-(15, '乐逍遥', '2人桌',3);
+INSERT INTO hp_table VALUES
+(1, '钙', '2人桌', 1),
+(2, '铁', '2人桌', 1),
+(3, '锌', '6人桌', 3),
+(5, '铜', '4人桌', 2),
+(6, '锰', '6人桌', 3),
+(7, '碘', '2人桌', 1),
+(8, '硅', '8人桌', 1),
+(9, '铬', '4人桌', 3),
+(10,'砷', '10人桌', 2),
+(11,'硒', '8人桌', 1),
+(12,'镁', '12人桌', 1),
+(13,'钒', '4人桌', 3),
+(15,'氟', '2人桌',3);
 
 /*桌台预定信息*/
-CREATE TABLE xfn_reservation(
+CREATE TABLE hp_reservation(
   rid INT PRIMARY KEY AUTO_INCREMENT,
   contactName VARCHAR(32),
   phone VARCHAR(16),
@@ -58,7 +58,7 @@ CREATE TABLE xfn_reservation(
   tableId INT,
   FOREIGN KEY(tableId) REFERENCES xfn_table(tid)
 );
-INSERT INTO xfn_reservation VALUES
+INSERT INTO hp_reservation VALUES
 (NULL, '丁丁', '13501234561', '1548311700000', '1549011000000', '1'),
 (NULL, '当当', '13501234562', '1548311710000', '1549011100000', '1'),
 (NULL, '豆豆', '13501234563', '1548311720000', '1549011200000', '2'),
@@ -82,11 +82,11 @@ INSERT INTO xfn_reservation VALUES
 (NULL, '丫丫', '13501234568', '1548311910000', '1549011200000', '15');
 
 /*菜品类别*/
-CREATE TABLE xfn_category(
+CREATE TABLE hp_category(
   cid INT PRIMARY KEY AUTO_INCREMENT,
   cname VARCHAR(32)
 );
-INSERT INTO xfn_category VALUES
+INSERT INTO hp_category VALUES
 (1, '肉类'),
 (2, '海鲜河鲜'),
 (3, '丸滑类'),
@@ -94,7 +94,7 @@ INSERT INTO xfn_category VALUES
 (5, '菌菇类');
 
 /*菜品*/
-CREATE TABLE xfn_dish(
+CREATE TABLE hp_dish(
   did INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(32),
   imgUrl VARCHAR(128),
@@ -103,7 +103,7 @@ CREATE TABLE xfn_dish(
   categoryId INT,
   FOREIGN KEY(categoryId) REFERENCES xfn_category(cid)
 );
-INSERT INTO xfn_dish VALUES
+INSERT INTO hp_dish VALUES
 (NULL, '草鱼片', 'r9470.jpg', '35', '选鲜活草鱼，切出鱼片冷鲜保存。锅开后再煮1分钟左右即可食用', '1'),
 (NULL, '脆皮肠', 'r9017.jpg', '15', '锅开后再煮3分钟左右即可食用', '1'),
 (NULL, '酥肉', 'r4760.jpg', '25', '选用冷鲜五花肉，加上鸡蛋，淀粉等原料炸制，色泽黄亮，酥软醇香，肥而不腻。锅开后再煮3分钟左右即可食用', '1'),
@@ -156,7 +156,7 @@ INSERT INTO xfn_dish VALUES
 (NULL, '金针菇', 'j6119.jpg', '28', '经过挑选、清洗、装盘而成。菌盖滑嫩、清脆，锅开后再煮2分钟左右即可食用', '5');
 
 /*订单*/
-CREATE TABLE xfn_order(
+CREATE TABLE hp_order(
   oid INT PRIMARY KEY AUTO_INCREMENT,
   startTime BIGINT,
   endTime BIGINT,
@@ -164,7 +164,7 @@ CREATE TABLE xfn_order(
   tableId INT,
   FOREIGN KEY(tableId) REFERENCES xfn_table(tid)
 );
-INSERT INTO xfn_order VALUES
+INSERT INTO hp_order VALUES
 (100000, '1547800000000', '1547814918000', '2', '1'),
 (NULL, '1547801000000', '1547824918000', '4', '2'),
 (NULL, '1547802000000', '1547834918000', '3', '3'),
@@ -193,7 +193,7 @@ INSERT INTO xfn_order VALUES
 (NULL, '1547802000000', '', '4', '15');
 
 /*订单详情*/
-CREATE TABLE xfn_order_detail(
+CREATE TABLE hp_order_detail(
   oid INT PRIMARY KEY AUTO_INCREMENT,
   dishId INT,       /*菜品编号*/
   dishCount INT,    /*份数*/
@@ -202,7 +202,7 @@ CREATE TABLE xfn_order_detail(
   FOREIGN KEY(dishId) REFERENCES xfn_dish(did),
   FOREIGN KEY(orderId) REFERENCES xfn_order(oid)
 );
-INSERT INTO xfn_order_detail VALUES
+INSERT INTO hp_order_detail VALUES
 (NULL, '50', '2', '丁丁', '100000'),
 (NULL, '24', '2', '丁丁', '100001'),
 (NULL, '37', '1', '当当', '100002'),
