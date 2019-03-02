c6b7c0557314c51634dcdd275a93f8e9fa3c4db7
weishaodaren <45391716+weishaodaren@users.noreply.github.com>
2019-03-02 16:03:26 +0800

    跨域

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const categoryRouter=require('./routes/admin/category');
 //主服务器
 var app=express();
-app.listen(3002,()=>{
+app.listen(6606,()=>{
     console.log(`服务器启动，全体注意...`)
 });
+//使用中间件
+app.use(cors());
+app.use(bodyParser.json());
+
+// 挂载路由器
+app.use('/admin/category',categoryRouter);
--- routes/admin/category.js
+++ routes/admin/category.js
@@ -2,4 +2,56 @@
 const express=require('express');
 const pool=require('../../pool.js');
 var router=express.Router();
+
+
+// 客户端获取所有菜品类别，按编号升序排列
+router.get('/',(req,res)=>{
+    pool.query('SELECT * FROM hp_category ORDER BY cid',(err,result)=>{
+        if(err) throw err;
+        res.send(result);
+    });
+});
+// 删除
+router.delete('/:cid',(req,res)=>{
+    //删除前，编号上设置为null
+    pool.query('UPDATE hp_dish SET categoryId=NULL WHERE categoryId=?',[req.params.cid],(err,result)=>{
+        if(err) throw err;
+        //指定类别菜品修改完毕
+    pool.query('DELETE FROM hp_category WHERE cid=?',[req.params.cid],(err,result)=>{
+        if(err) throw err;
+        if(result.affectedRows>0){
+            res.send({code:200,msg:`1 category deleted`});
+        }else{
+            res.send({code:400,msg:`0 category deleted`});
+        }
+    });
+    });
+//添加
+router.post('/',(req,res)=>{
+    // console.log(`获取到请求数据:`);
+    // console.log(req.body);
+    var data=req.body;
+    pool.query('INSERT INTO hp_category SET ?',[data],(err,result)=>{
+        if(err) throw err;
+        res.send({code:200,msg:`1 category added`});
+    });
+
+});
+});
+//修改
+router.put('/',(req,res)=>{
+    var data=req.body;
+    pool.query('UPDATE hp_category SET ? WHERE cid=?',[data,data.cid],(err,result)=>{
+        if(err) throw err;
+        if(result.changedRows>0){
+            res.send({code:200,msg:`1 category updated`});
+        }else if(result.affectedRows==0){
+            res.send({code:400,msg:`category not exists`});
+        }else if(result.affectedRows==1&&result.changedRows==0){
+            res.send({code:401,msg:`no category modified`});
+        }
+    });
+});
+
+
 module.exports=router;
